d8ee88959a91c9cbe932c8d8f034a90a5426e357
jeikerxiao <jeiker@126.com>
2018-03-07 15:07:11 +0800

    change hello project name
    change hello project name

--- springboot2-hello/src/main/java/com/jeiker/hello/Springboot2HelloApplication.java
+++ springboot2-hello/src/main/java/com/jeiker/hello/Springboot2HelloApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoHelloApplication {
+public class Springboot2HelloApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoHelloApplication.class, args);
+		SpringApplication.run(Springboot2HelloApplication.class, args);
 	}
 }
