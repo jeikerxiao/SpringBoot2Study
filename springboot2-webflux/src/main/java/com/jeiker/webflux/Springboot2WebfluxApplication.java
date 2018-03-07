b4d284ecdd04a6a8a609d3674ddd0fd7604d32ab
jeikerxiao <jeiker@126.com>
2018-03-07 14:25:42 +0800

    init webflux project
    init webflux project

---
+++ springboot2-webflux/src/main/java/com/jeiker/webflux/Springboot2WebfluxApplication.java
@@ -0,0 +1,12 @@
+package com.jeiker.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2WebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2WebfluxApplication.class, args);
+	}
+}
